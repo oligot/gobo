44a03078360f48269dfd33b33dba6986954260cf
Olivier Ligot <oligot@gmail.com>
2013-02-26 14:42:37 +0100

    Install lexical and parse libraries

--- epm.bat
+++ epm.bat
@@ -3,5 +3,9 @@ set cwd=%cd%
 
 cd %GOBO%\library\kernel
 geant install
+cd %GOBO%\library\lexical
+geant install
+cd %GOBO%\library\parse
+geant install
 
 cd %cwd%
--- epm.sh
+++ epm.sh
@@ -3,3 +3,7 @@
 echo "Building GOBO..."
 cd $GOBO/library/kernel
 geant install
+cd $GOBO/library/lexical
+geant install
+cd $GOBO/library/parse
+geant install
